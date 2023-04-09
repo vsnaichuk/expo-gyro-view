8c58e5a995f6c61a2fc57f8c927c51c8441a2d10
vsnaichuk <v.snaichuk@gmail.com>
2023-04-10 01:26:12 +0300

    Remove web files. Implement native Gyro View

---
+++ ios/GyroView.swift
@@ -0,0 +1,70 @@
+//
+//  GyroView.swift
+//  ExpoGyroView
+//
+//  Created by vsn on 10.04.2023.
+//
+
+import ExpoModulesCore
+import UIKit
+import CoreMotion
+
+class GyroView: UIView {
+  let motion = CMMotionManager()
+  var onGyroEvent: EventDispatcher? = nil
+  
+  var timer: Timer? = nil
+  
+  lazy var yPositionTextView = UILabel()
+  
+  func setPlaceholderText(_ text: String?) {
+    self.yPositionTextView.text = text ?? "Start"
+  }
+  
+  func setEventDispatcher(_ eventDispatcher: EventDispatcher) {
+    self.onGyroEvent = eventDispatcher
+  }
+  
+  func startGyros() {
+    if motion.isGyroAvailable {
+      self.motion.gyroUpdateInterval = 30.0 / 60.0
+      self.motion.startAccelerometerUpdates()
+      
+      self.timer = Timer(fire: Date(), interval: (30.0/60.0), repeats: true, block: { (timer) in
+        if let data = self.motion.accelerometerData {
+          let x = data.acceleration.x
+          let y = data.acceleration.y
+          let z = data.acceleration.z
+          self.yPositionTextView.text = "\(round(y * 100) / 100.0)"
+          self.onGyroEvent?([
+            "y": y
+          ])
+        }
+      })
+      
+      RunLoop.current.add(self.timer!, forMode: .default)
+    }
+  }
+  
+  func stopGyros() {
+    if self.timer != nil {
+      self.timer?.invalidate()
+      self.timer = nil
+      self.motion.stopGyroUpdates()
+    }
+  }
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    self.addSubview(yPositionTextView)
+    self.yPositionTextView.translatesAutoresizingMaskIntoConstraints = false
+    self.yPositionTextView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+    self.yPositionTextView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+    self.yPositionTextView.font = UIFont.systemFont(ofSize: 100)
+    self.yPositionTextView.text = "START"
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,9 @@
-import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-core';
-
-// Import the native module. On web, it will be resolved to ExpoGyroView.web.ts
-// and on native platforms to ExpoGyroView.ts
-import ExpoGyroViewModule from './ExpoGyroViewModule';
-import ExpoGyroView from './ExpoGyroView';
-import { ChangeEventPayload, ExpoGyroViewProps } from './ExpoGyroView.types';
-
-// Get the native constant value.
-export const PI = ExpoGyroViewModule.PI;
-
+// For test
 export function hello(): string {
-  return ExpoGyroViewModule.hello();
+  return 'hello';
 }
 
-export async function setValueAsync(value: string) {
-  return await ExpoGyroViewModule.setValueAsync(value);
-}
-
-const emitter = new EventEmitter(ExpoGyroViewModule ?? NativeModulesProxy.ExpoGyroView);
-
-export function addChangeListener(listener: (event: ChangeEventPayload) => void): Subscription {
-  return emitter.addListener<ChangeEventPayload>('onChange', listener);
-}
-
-export { ExpoGyroView, ExpoGyroViewProps, ChangeEventPayload };
+export {
+  default as ExpoGyroView,
+  Props as ExpoGyroViewProps
+} from './ExpoGyroView';
